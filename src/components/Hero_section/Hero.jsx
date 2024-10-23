b3c1bfd55cd9c05ceff36158fce59a47c83ce934
Alejandra Rodriguez <alejandra.franco1290@gmail.com>
2024-10-22 21:44:29 -0500

    Hero Responsive

--- src/components/Hero_section/Hero.jsx
+++ src/components/Hero_section/Hero.jsx
@@ -4,13 +4,14 @@ import Image2 from "../../assets/Hero/image2.jpg";
 import Image3 from "../../assets/Hero/image3.jpg";
 import Image4 from "../../assets/Hero/image4.png";
 import Slider from "react-slick";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+
 const ImageList = [
   {
     id: 1,
     img: Image1,
-    title: " Much more than just a generic vision",
-    description: "Integrated Precision"
-
+    title: "Much more than just a generic vision",
+    description: "Integrated Precision",
   },
   {
     id: 2,
@@ -18,18 +19,19 @@ const ImageList = [
   },
   {
     id: 3,
-    img: Image3
+    img: Image3,
   },
   {
     id: 4,
-    img: Image4
+    img: Image4,
   },
+];
 
-
-]
 const Hero = () => {
-  var settings = {
-    dots: false,
+  const sliderRef = React.useRef(null);
+
+  const settings = {
+    dots: true,
     arrows: false,
     infinite: true,
     speed: 800,
@@ -39,14 +41,23 @@ const Hero = () => {
     cssEase: "ease-in-out",
     pauseOnHover: false,
     pauseOnFocus: true,
+    dotsClass: "slick-dots custom-dots",
+  };
+
+  const nextSlide = () => {
+    sliderRef.current.slickNext();
+  };
+
+  const prevSlide = () => {
+    sliderRef.current.slickPrev();
   };
 
   return (
-    <div className='relative overflow-hidden  bg-slate-400'>
+    <div className="relative overflow-hidden bg-slate-400">
       {/* Hero Section */}
-      <Slider {...settings}>
+      <Slider ref={sliderRef} {...settings}>
         {ImageList.map((data) => (
-          <div className="relative">
+          <div key={data.id} className="relative">
             {/* image section */}
             <div className="relative z-0">
               <img
@@ -58,18 +69,30 @@ const Hero = () => {
 
             {/* text content section */}
             <div className="absolute right-0 top-1/4 sm:top-1/3 z-10 text-white p-4 sm:p-8">
-              <h1 className="text-xl sm:text-2xl font-bold text-right">
+              <h1 className="text-lg sm:text-2xl font-bold text-right">
                 {data.title}
               </h1>
-              <p className="text-5xl sm:text-8xl font-bold text-right">
+              <p className="text-3xl sm:text-5xl font-bold text-right">
                 {data.description}
               </p>
             </div>
           </div>
         ))}
       </Slider>
+
+      {/* Navigation Arrows */}
+      <div className="absolute top-1/2 left-4 transform -translate-y-1/2 z-20">
+        <button onClick={prevSlide} className="bg-primary text-white p-2 rounded-full">
+          <FaArrowLeft />
+        </button>
+      </div>
+      <div className="absolute top-1/2 right-4 transform -translate-y-1/2 z-20">
+        <button onClick={nextSlide} className="bg-primary text-white p-2 rounded-full">
+          <FaArrowRight />
+        </button>
+      </div>
     </div>
   );
 };
 
-export default Hero
+export default Hero;
